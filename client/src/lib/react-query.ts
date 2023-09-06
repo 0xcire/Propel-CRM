bbf6c447a4afb92d7013252c6305cdab40ef063b
0xcire <ciredeveloper@gmail.com>
2023-09-05 21:18:10 -0400

    aside: addressing docker issue with vite

--- client/src/lib/react-query.ts
+++ client/src/lib/react-query.ts
@@ -4,15 +4,16 @@ import { QueryClient, type DefaultOptions } from '@tanstack/react-query';
 
 const queryConfig: DefaultOptions = {
   queries: {
-    refetchOnWindowFocus: false,
+    refetchOnWindowFocus: true,
     retry: false,
-    onError: (error) => {
-      if (isAPIError(error)) {
-        return toast({
-          description: `${error.message}`,
-        });
-      }
-    },
+    // TODO: implement react query auth myself to revert this comment
+    // onError: (error) => {
+    //   if (isAPIError(error)) {
+    //     return toast({
+    //       description: `${error.message}`,
+    //     });
+    //   }
+    // },
     useErrorBoundary: (error) => isAPIError(error) && error.status >= 500,
   },
   mutations: {
