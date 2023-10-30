f664cc5423aa92d8582c899a67a7d0f071ea32e7
0xcire <ciredeveloper@gmail.com>
2023-10-30 18:48:43 -0400

    fix: prod build config

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,6 +13,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  build: {
+    sourcemap: false,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
