0a766cb2851770feee31514fc90b820e1764139d
0xcire <ciredeveloper@gmail.com>
2023-11-14 23:45:50 -0500

    feat: contacts route, links to related listings/tasks

---
+++ client/src/features/contacts/components/page/ContactListings.tsx
@@ -0,0 +1,53 @@
+import { Link } from 'react-router-dom';
+
+import { ScrollArea } from '@/components/ui/scroll-area';
+import { Typography } from '@/components/ui/typography';
+
+import { Spinner } from '@/components';
+
+import type { UseQueryResult } from '@tanstack/react-query';
+import type { Listings } from '@/features/listings/types'; //?
+
+interface ContactListingProps {
+  listings: UseQueryResult<Listings, unknown>;
+}
+
+export function ContactListings({
+  listings,
+}: ContactListingProps): JSX.Element {
+  if (listings.isLoading) {
+    return <Spinner fillContainer />;
+  }
+
+  return (
+    <>
+      <div className='flex h-[60px] items-center'>
+        <Typography
+          variant='h4'
+          className='text-lg'
+        >
+          Interested Listings
+        </Typography>
+      </div>
+
+      <ScrollArea className='h-[calc(100%-60px)] w-full'>
+        {listings.data?.length === 0 ? (
+          <p className='text-sm text-muted-foreground'>
+            Not interested in any listings right now.
+          </p>
+        ) : (
+          listings.data?.map((listing) => (
+            <div key={`listing-${listing.id}`}>
+              <Link
+                className='text-sm transition-colors duration-200 hover:text-muted-foreground'
+                to={`/listings/?address=${listing.address}`}
+              >
+                {listing.address}
+              </Link>
+            </div>
+          ))
+        )}
+      </ScrollArea>
+    </>
+  );
+}
---
+++ client/src/features/contacts/components/page/ContactTasks.tsx
@@ -0,0 +1,53 @@
+import { Link } from 'react-router-dom';
+
+import { useUser } from '@/lib/react-query-auth';
+
+import { ScrollArea } from '@/components/ui/scroll-area';
+
+import { Spinner } from '@/components';
+import { Task } from '@/features/tasks/components/Task'; // one off
+
+import type { UseQueryResult } from '@tanstack/react-query';
+import type { Tasks } from '@/features/tasks/types';
+
+type ContactTasksProps = {
+  tasks: UseQueryResult<Tasks, unknown>;
+  contactID: number;
+};
+
+export function ContactTasks({
+  tasks,
+  contactID,
+}: ContactTasksProps): JSX.Element {
+  const user = useUser();
+
+  if (tasks.isLoading) {
+    return <Spinner fillContainer />;
+  }
+
+  return (
+    <>
+      <div className='flex h-[60px] items-center'>
+        <Link
+          className='text-lg font-bold'
+          to={`/tasks/contacts/${contactID}?page=1&limit=10&completed=false`}
+        >
+          Upcoming Tasks
+        </Link>
+      </div>
+      <ScrollArea className='h-[calc(100%-60px)] w-full'>
+        {tasks.data?.length === 0 ? (
+          <p className='text-sm text-muted-foreground'>You're all caught up!</p>
+        ) : (
+          tasks.data?.map((task) => (
+            <Task
+              key={`task-${task.id}`}
+              task={task}
+              userID={user.data?.id as number}
+            />
+          ))
+        )}
+      </ScrollArea>
+    </>
+  );
+}
