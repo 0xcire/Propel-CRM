ef301398e38ba6848d1b3e5941dbea4f6d4228d7
0xcire <ciredeveloper@gmail.com>
2023-12-13 21:53:40 -0500

    feat: automation scripts for build / deploy processes

--- scripts/s3-sync.sh
+++ scripts/s3-sync.sh
@@ -4,4 +4,16 @@ cd ..
 
 cd client
 
-aws s3 sync --delete dist s3://propel-static
+if [ -d dist ]; then
+{
+    echo "removing previous build"
+    rm -rf dist
+}
+fi
+
+npm run build
+
+aws s3 sync --delete dist $CLIENT_S3_BUCKET
+aws cloudfront create-invalidation \
+    --distribution-id $CLOUDFRONT_DISTRIBUTION \
+    --paths "/*"
