9bfdcdcfabf2031a6433a4d46c6e52c3f2e0e026
0xcire <ciredeveloper@gmail.com>
2024-01-06 17:54:22 -0500

    fix: change playwright config.webServer.url to actually fix race condition

--- apps/e2e/playwright.config.ts
+++ apps/e2e/playwright.config.ts
@@ -1,38 +1,21 @@
 import { defineConfig, devices } from "@playwright/test";
+import { CI } from "./playwright/config";
 
-// consider pulling out to root dir if more packages depend on playwright
+const isCI = !!CI;
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-// process.env.CI
+/* See https://playwright.dev/docs/test-configuration. */
 
-const isCI = !!process.env.CI;
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
   testDir: "./playwright",
   testMatch: "*/*.e2e.ts",
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: isCI,
-  /* Retry on CI only. Currently modified to retry once, locally, just testing for now.. */
+  fullyParallel: true /* Run tests in files in parallel */,
+  forbidOnly: isCI /* test.only */,
   retries: isCI ? 1 : 0,
-  /* Opt out of parallel tests on CI. */
   workers: isCI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  reporter: "html" /* See https://playwright.dev/docs/test-reporters */,
+  /*  See https://playwright.dev/docs/api/class-testoptions. */
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:8080",
-
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
 
@@ -52,32 +35,15 @@ export default defineConfig({
     //   name: "webkit",
     //   use: { ...devices["Desktop Safari"] },
     // },
-
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
-
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
   ],
 
   /* Run your local dev server before starting the tests */
   webServer: {
     command: "cd ../../.docker && docker compose -f docker-compose.dev.yml up",
-    url: "http://localhost:8080",
+    url: "http://localhost:8080/api/user/me", // return 403 -> start tests
     reuseExistingServer: !isCI,
+    timeout: 500000, // arbitrarily large, incase docker env needs to be built
   },
+  // globalSetup: "./playwright/globals/setup.ts",
+  globalTeardown: "./playwright/globals/teardown.ts",
 });
