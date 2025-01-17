b2bb1ea2c57d0251d310c8ab10574d6f3c14b435
0xcire <ciredeveloper@gmail.com>
2025-01-17 15:24:50 -0500

    delete refactored code

--- apps/server/src/controllers/auth/index.ts
+++
@@ -1,5 +0,0 @@
-export { signin } from "./signin";
-export { signup } from "./signup";
-export { signout } from "./signout";
-export * from "./recover-password";
-export * from "./verify-email";
--- apps/server/src/controllers/types.ts
+++
@@ -1,9 +0,0 @@
-import type { User } from "@propel/drizzle";
-
-export type UserResponse = Partial<User>;
-export type UserInput = {
-  name?: string | undefined;
-  username?: string | undefined;
-  email: string | undefined;
-  password: string | undefined;
-};
